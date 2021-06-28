d6250baa3a5f0ea1d65b28ee3f048256f6489326
ClaySamuelC <44789280+ClaySamuelC@users.noreply.github.com>
2021-06-28 02:03:21 -0700

    Add reactive turns

--- PacMan/Source.cpp
+++ PacMan/Source.cpp
@@ -1,6 +1,8 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+#include <cmath>
+
 class PacMan : public olc::PixelGameEngine
 {
 public:
@@ -9,27 +11,32 @@ public:
 		sAppName = "PacMan";
 	}
 
+	const float fVelocity = 3.5;
+
 	const uint32_t nTileWidth = 14;
 	const uint32_t nPelletRadius = 2;
 	const uint32_t nPacRadius = 7;
-	const float fVelocity = 2.5;
 
-	std::vector<std::string> vRoom;
+	uint32_t roomWidth;
+	uint32_t roomHeight;
+	std::string vRoom [31];
 
 	olc::Pixel wallColor = olc::BLUE;
 	olc::Pixel dotColor = olc::WHITE;
-	olc::Pixel pacColor;
-	olc::Pixel ghostColor;
+	olc::Pixel pacColor = olc::YELLOW;
 	olc::Pixel backgroundColor = olc::BLACK;
 
-	uint32_t roomWidth;
+	olc::Pixel ghostColor;
+
 	olc::vd2d vPacLoc;
-	uint8_t nPacDir; // 0: left, 1: up, 2: right, 3: down
+	olc::vd2d vPacDir;
+	olc::vd2d vNextTurn;
+	olc::vd2d vMoveDiff;
 
 public:
 	void drawPac()
 	{
-		FillCircle(olc::vi2d(uint32_t(vPacLoc.x * nTileWidth + nTileWidth / 2), uint32_t(vPacLoc.y * nTileWidth + nTileWidth / 2)), nPacRadius, olc::YELLOW);
+		FillCircle(olc::vi2d(uint32_t(vPacLoc.x * nTileWidth + nTileWidth / 2), uint32_t(vPacLoc.y * nTileWidth + nTileWidth / 2)), nPacRadius, pacColor);
 	}
 
 	void drawRoom()
@@ -48,53 +55,75 @@ public:
 				case '.':
 					FillCircle(vPos + olc::vi2d(nTileWidth / 2, nTileWidth / 2), nPelletRadius, olc::WHITE);
 					break;
-				case ' ':
-					FillRect(vPos, olc::vi2d(nTileWidth, nTileWidth), olc::BLACK);
-					break;
 				}
 			}
 			y++;
 		}
 	}
 
+	double findNextTurn()
+	{
+		if (vPacDir.x < 0)
+			for (int i = vPacLoc.x; i > 0; i--)
+				if (vRoom[int(vPacLoc.y + vNextTurn.y)][i] != '/')
+					return i - vPacLoc.x;
+		else if (vPacDir.y < 0)
+			for (int i = vPacLoc.y; i > 0; i--)
+				if (vRoom[i][vPacLoc.x + vNextTurn.x] != '/')
+					return i - vPacLoc.y;
+		else if (vPacDir.x > 0)
+			for (int i = vPacLoc.x; i < roomWidth; i++)
+				if (vRoom[int(vPacLoc.y + vNextTurn.y)][i] != '/')
+					return vPacLoc.x - i;
+		else if (vPacDir.y > 0)
+			for (int i = vPacLoc.y; i < roomHeight; i++)
+				if (vRoom[i][vPacLoc.x + vNextTurn.x] != '/')
+					return vPacLoc.y - i;
+
+		return -1;
+	}
+
 	bool OnUserCreate() override
 	{
 		roomWidth = 28;
+		roomHeight = 31;
+
 		vPacLoc = olc::vd2d(13.5, 23.0);
-		nPacDir = 2;
+		vPacDir = olc::vd2d(0.0, 0.0);
+		vNextTurn = olc::vd2d(0.0, 0.0);
 
 		// Width: 28, Height: 31
-		vRoom.push_back("////////////////////////////");
-		vRoom.push_back("/............//............/");
-		vRoom.push_back("/.////./////.//./////.////./");
-		vRoom.push_back("/.////./////.//./////.////./");
-		vRoom.push_back("/.////./////.//./////.////./");
-		vRoom.push_back("/........................../");
-		vRoom.push_back("/.////.//.////////.//.////./");
-		vRoom.push_back("/.////.//.////////.//.////./");
-		vRoom.push_back("/......//....//....//....../");
-		vRoom.push_back("//////.///// // /////.//////");
-		vRoom.push_back("//////.///// // /////.//////");
-		vRoom.push_back("//////.//          //.//////");
-		vRoom.push_back("//////.// //////// //.//////");
-		vRoom.push_back("//////.// /      / //.//////");
-		vRoom.push_back("      .   /      /   .      ");
-		vRoom.push_back("//////.// /      / //.//////");
-		vRoom.push_back("//////.// //////// //.//////");
-		vRoom.push_back("//////.//          //.//////");
-		vRoom.push_back("//////.// //////// //.//////");
-		vRoom.push_back("//////.// //////// //.//////");
-		vRoom.push_back("/............//............/");
-		vRoom.push_back("/.////./////.//./////.////./");
-		vRoom.push_back("/.////./////.//./////.////./");
-		vRoom.push_back("/ ..//.......  .......//.. /");
-		vRoom.push_back("///.//.//.////////.//.//.///");
-		vRoom.push_back("///.//.//.////////.//.//.///");
-		vRoom.push_back("/......//....//....//....../");
-		vRoom.push_back("/.//////////.//.//////////./");
-		vRoom.push_back("/.//////////.//.//////////./");
-		vRoom.push_back("/........................../");
-		vRoom.push_back("////////////////////////////");
+		vRoom[0]  = "////////////////////////////";
+		vRoom[1]  = "/............//............/";
+		vRoom[2]  = "/.////./////.//./////.////./";
+		vRoom[3]  = "/.////./////.//./////.////./";
+		vRoom[4]  = "/.////./////.//./////.////./";
+		vRoom[5]  = "/........................../";
+		vRoom[6]  = "/.////.//.////////.//.////./";
+		vRoom[7]  = "/.////.//.////////.//.////./";
+		vRoom[8]  = "/......//....//....//....../";
+		vRoom[9]  = "//////.///// // /////.//////";
+		vRoom[10] = "//////.///// // /////.//////";
+		vRoom[11] = "//////.//          //.//////";
+		vRoom[12] = "//////.// //////// //.//////";
+		vRoom[13] = "//////.// //////// //.//////";
+		vRoom[14] = "      .   //    //   .      ";
+		vRoom[15] = "//////.// //////// //.//////";
+		vRoom[16] = "//////.// //////// //.//////";
+		vRoom[17] = "//////.//          //.//////";
+		vRoom[18] = "//////.// //////// //.//////";
+		vRoom[19] = "//////.// //////// //.//////";
+		vRoom[20] = "/............//............/";
+		vRoom[21] = "/.////./////.//./////.////./";
+		vRoom[22] = "/.////./////.//./////.////./";
+		vRoom[23] = "/ ..//.......  .......//.. /";
+		vRoom[24] = "///.//.//.////////.//.//.///";
+		vRoom[25] = "///.//.//.////////.//.//.///";
+		vRoom[26] = "/......//....//....//....../";
+		vRoom[27] = "/.//////////.//.//////////./";
+		vRoom[28] = "/.//////////.//.//////////./";
+		vRoom[29] = "/........................../";
+		vRoom[30] = "////////////////////////////";
 
 		return true;
 	}
@@ -106,76 +135,80 @@ public:
 		drawRoom();
 		drawPac();
 
-		// movement logic
-		double fPacMov = fVelocity * double(fElapsedTime);
-		olc::vd2d vPacMov = olc::vd2d(0.0, 0.0);
+		// movement
+		olc::vd2d vPacMov = vPacDir * (fVelocity * double(fElapsedTime));
 
-		switch (nPacDir)
-		{
-		case 0:
-			vPacMov.x = -fPacMov;
-			break;
-		case 1:
-			vPacMov.y = -fPacMov;
-			break;
-		case 2:
-			vPacMov.x = fPacMov;
-			break;
-		case 3:
-			vPacMov.y = fPacMov;
-			break;
-		}
+		olc::vd2d vPacOldLoc = olc::vd2d(vPacLoc.x, vPacLoc.y);
 
 		vPacLoc += vPacMov;
 
 		// check collisions
-		switch (nPacDir)
-		{
-		case 0:
-			if (vRoom.at(int(vPacLoc.y))[int(vPacLoc.x)] == '/')
-				vPacLoc.x = double(int(vPacLoc.x) + 1);
-			break;
-		case 1:
-			if (vRoom.at(int(vPacLoc.y))[int(vPacLoc.x)] == '/')
-				vPacLoc.y = double(int(vPacLoc.y) + 1);
-			break;
-		case 2:
-			if (vRoom.at(int(vPacLoc.y))[int(vPacLoc.x) + 1] == '/')
-			vPacLoc.x = double(int(vPacLoc.x));
-			break;
-		case 3:
-			if (vRoom.at(int(vPacLoc.y) + 1)[int(vPacLoc.x)] == '/')
-			vPacLoc.y = double(int(vPacLoc.y));
-			break;
-		}
+		if		(vPacDir.x < 0 && vRoom[int(vPacLoc.y)][vPacLoc.x] == '/')
+			vPacLoc.x = std::ceil(vPacLoc.x);
+		else if (vPacDir.y < 0 && vRoom[int(vPacLoc.y)][vPacLoc.x] == '/')
+			vPacLoc.y = std::ceil(vPacLoc.y);
+		else if (vPacDir.x > 0 && vRoom[int(vPacLoc.y)][std::ceil(vPacLoc.x)] == '/')
+			vPacLoc.x = std::floor(vPacLoc.x);
+		else if (vPacDir.y > 0 && vRoom[int(std::ceil(vPacLoc.y))][vPacLoc.x] == '/')
+			vPacLoc.y = std::floor(vPacLoc.y);
 
 		// input logic
-		if (GetKey(olc::Key::A).bReleased)
-		{
-			nPacDir = 0;
-		}
-		else if (GetKey(olc::Key::W).bReleased)
-		{
-			nPacDir = 1;
-		}
-		else if (GetKey(olc::Key::D).bReleased)
-		{
-			nPacDir = 2;
-		}
-		else if (GetKey(olc::Key::S).bReleased)
-		{
-			nPacDir = 3;
-		}
+		if (GetKey(olc::Key::A).bHeld)
+			vNextTurn = olc::vd2d(-1, 0);
+		else if (GetKey(olc::Key::W).bHeld)
+			vNextTurn = olc::vd2d(0, -1);
+		else if (GetKey(olc::Key::D).bHeld)
+			vNextTurn = olc::vd2d(1, 0);
+		else if (GetKey(olc::Key::S).bHeld)
+			vNextTurn = olc::vd2d(0, 1);
+		else if (GetKey(olc::Key::SPACE).bPressed)
+			std::cout << vPacLoc.x << ", " << vPacLoc.y << ", " << vPacDir.x << ", " << vPacDir.y
+			<< ", " << vNextTurn.x << ", " << vNextTurn.y << std::endl;
 
+		bool bCanTurn = (vPacLoc.x == std::floor(vPacLoc.x) && vPacLoc.y == std::floor(vPacLoc.y))
+			|| (std::floor(vPacLoc.x) != std::floor(vPacOldLoc.x) || std::floor(vPacLoc.y) != std::floor(vPacOldLoc.y));
+
+		// turn logic
+		if (vNextTurn.x == vPacDir.x || vNextTurn.y == vPacDir.y)
+			vPacDir = vNextTurn;
+		else if (bCanTurn && vNextTurn != vPacDir)
+		{
+			double fMoveDiff = std::abs(vPacLoc.x - std::round(vPacLoc.x) + vPacLoc.y - std::round(vPacLoc.y));
+
+			if		(vPacDir.x < 0) vPacLoc.x = std::ceil(vPacLoc.x);
+			else if (vPacDir.y < 0) vPacLoc.y = std::ceil(vPacLoc.y);
+			else if (vPacDir.x > 0) vPacLoc.x = std::floor(vPacLoc.x);
+			else if (vPacDir.y > 0) vPacLoc.y = std::floor(vPacLoc.y);
+
+			int nPacX = vPacLoc.x;
+			int nPacY = vPacLoc.y;
+			
+			if ((vPacDir.x < 0															// going left
+				&& (vNextTurn.y < 0 && vRoom[nPacY - 1][std::ceil(vPacLoc.x)] != '/'	// turning down
+				||  vNextTurn.y > 0 && vRoom[nPacY + 1][std::ceil(vPacLoc.x)] != '/'))	// turning up
+			|| (vPacDir.y < 0															// going up
+				&& (vNextTurn.x < 0 && vRoom[nPacY][std::ceil(vPacLoc.x - 1)] != '/'	// turning left
+				||  vNextTurn.x > 0 && vRoom[nPacY][std::ceil(vPacLoc.x + 1)] != '/'))	// turning right
+			|| (vPacDir.x > 0															// going right
+				&& (vNextTurn.y < 0 && vRoom[nPacY - 1][vPacLoc.x] != '/'				// turning down
+				||  vNextTurn.y > 0 && vRoom[nPacY + 1][vPacLoc.x] != '/'))				// turning up
+			|| (vPacDir.y > 0															// going down
+				&& (vNextTurn.x < 0 && vRoom[nPacY][vPacLoc.x - 1] != '/'				// turning left
+				||  vNextTurn.x > 0 && vRoom[nPacY][vPacLoc.x + 1] != '/')))			// turning right
+			{
+				vPacLoc += vNextTurn * fMoveDiff;
+				vPacDir = vNextTurn;
+			}
+		}
+		
 		return true;
 	}
 };
 
-
 int main()
 {
 	PacMan demo;
-	if (demo.Construct(392, 434, 1, 1))
+	if (demo.Construct(392, 434, 2, 2))
 		demo.Start();
 
 	return 0;
